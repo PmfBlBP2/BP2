e4296f83888e7b3abe62df86958523762645a7f1
Vladimir Filipovic <vladofilipovic@hotmail.com>
2019-04-25 10:16:57 +0200

    dodao projekat

--- predavanja/primeri-dotnetcore/HelloWorld/HelloConsoleEF/Program.cs
+++ predavanja/primeri-dotnetcore/HelloWorld/HelloConsoleEF/Program.cs
@@ -1,21 +1,83 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Text;
 
 namespace HelloConsoleEF
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void PrikaziGradove()
         {
-            Console.WriteLine("Prikaz podataka iz baze:");
-            PrintData();
-            Console.Read();
+            using (var context = new ProbaContext())
+            {
+                var listaGrad = context.Grad;
+                foreach (var g in listaGrad)
+                {
+                    var data = new StringBuilder();
+                    data.AppendLine($"Naziv grada: {g.Naziv}");
+                    data.AppendLine($"Id grada: {g.Id}");
+                    Console.WriteLine(data.ToString());
+                }
+            }
         }
 
-        private static void PrintData()
+ 
+        static void PrikaziMeni()
         {
-            // Gets and prints all books in database
+            Console.WriteLine("Izaberi opciju:");
+            Console.WriteLine("1. Prikaz podataka o svim skolama");
+            Console.WriteLine("2. Prikaz podataka o skolama iz datog grada");
+            Console.WriteLine("3. Dodavanje grada");
+            Console.WriteLine("4. Dodavanje skole");
+            Console.WriteLine("x. Izlaz");
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                PrikaziMeni();
+                char izbor = Console.ReadLine().Trim().ToUpper()[0];
+                if (izbor == 'X')
+                    return;
+                switch (izbor)
+                {
+                    case '1':
+                        Console.WriteLine("Prikaz podataka iz baze:");
+                        IspisiPodatke();
+                        break;
+                    case '2':
+                        Console.WriteLine("Unesite ime grada.");
+                        string imeGrada = Console.ReadLine();
+                        Console.WriteLine($"\nSkole iz grada {imeGrada} su: \n");
+                        IspisiSkole(imeGrada);
+                        break;
+                    case '3': 
+                        Console.WriteLine("Unesite ime grada.");
+                        string imeGrada1 = Console.ReadLine();
+                        DodajGrad(imeGrada1);
+                        break;
+                    case '4':
+                        Console.WriteLine("Unesite ime skole.");
+                        string imeSkole = Console.ReadLine();
+                        Console.WriteLine("Unesite adresu skole.");
+                        string adresaSkole = Console.ReadLine();
+                        PrikaziGradove();
+                        Console.WriteLine("Unesite id grada koji je ponudjen.");
+                        int idGrada = Convert.ToInt32(Console.ReadLine());
+                        DodajSkolu(imeSkole, adresaSkole, idGrada);
+                        break;
+                    default:
+                        Console.WriteLine("Opcija ne postoji.");
+                        break;
+                }
+            }
+        }
+
+        private static void IspisiPodatke()
+        {
+         
             using (var context = new ProbaContext())
             {
                 var skole = context.Skola.Include(p => p.Grad);
@@ -30,5 +92,48 @@ namespace HelloConsoleEF
             }
 
         }
+
+        private static void IspisiSkole(string imeGrada)
+        {
+            using (var context = new ProbaContext())
+            {
+                var listaGradId = context.Grad.Where(x => x.Naziv.Equals(imeGrada))
+                .Select(x => x.Id).ToList();
+                int gradId = listaGradId[0];
+
+                var skole = context.Skola.Where(x => x.GradId == gradId)
+                .Include(x => x.Grad);
+
+                foreach (var s in skole)
+                {
+                    var data = new StringBuilder();
+                    data.AppendLine($"Naziv: {s.Naziv}");
+                    data.AppendLine($"Adresa: {s.Adresa}");
+                    Console.WriteLine(data.ToString());
+                }
+            }
+
+        }
+
+        private static void DodajGrad(string imeGrada){
+            using(var context = new ProbaContext()){
+                Grad g = new Grad();
+                g.Naziv = imeGrada;
+                context.Grad.Add(g);
+                context.SaveChanges();
+            }
+        }
+
+        private static void DodajSkolu(string imeSkole, string adresaSkole, int idGrada){
+            using(var context = new ProbaContext()){
+                Skola s = new Skola();
+                s.Naziv = imeSkole;
+                s.Adresa = adresaSkole;
+                s.GradId = idGrada;
+                context.Skola.Add(s);
+                context.SaveChanges();
+            }
+        }
+
     }
 }
